becdbe90bdfb896eec1270b69d072e90fb19b053
gary <gary@ligginsdevelopment.com>
2021-03-25 15:00:50 -0700

    everything done css will finish later

--- post.js
+++ post.js
@@ -1,28 +1,25 @@
 const API_URL = "http://localhost:3000/api/posts/";
-const API_BASE_URL = 'http://localhost:3000/';
+const API_BASE_URL = "http://localhost:3000/";
 
 window.onload = () => {
-    getPosts();
-    getPostIdParam()
+    getPost();
 }
 
-getPostIdParam = () => {
-    const queryString = window.location.search
-    const urlParams = new URLSearchParams(queryString)
-    return urlParams.get("id")
+const getPostIdParam = () => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    return urlParams.get('id');
 }
 
-const getPosts = () => {
-    const postId = getPostIdParam();
-    const url = `${API_URL}${postId}`
+const getPost = () => {
+    const url = `${API_URL}${getPostIdParam()}`
     fetch(url, {
-        method: "GET"
-    }).then((response) => {
-        return response.json()
-    }).then((data) => {
-        buildPost(data)
+        method: 'GET'
+    }).then((response)=>{
+        return response.json();
+    }).then((data)=>{
+        buildPost(data);
     })
-
 }
 
 const buildPost = (data) => {
